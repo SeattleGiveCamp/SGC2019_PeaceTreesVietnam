fee7efe1639eae5484589cce7a76c00f9d747d57
Geoffrey Liu <geliu@groupon.com>
2019-10-19 11:05:39 -0700

    add two-way binding for login form state

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -1,9 +1,14 @@
 import React from "react";
+import ReactDOM from "react-dom";
+
+import LoginForm from "../../components/LoginForm/LoginForm.js";
 
 export default class Login extends React.Component {
   state = {};
 
   render() {
-    return <div>Login</div>;
+    return <div>
+      <LoginForm></LoginForm>
+    </div>
   }
 }
