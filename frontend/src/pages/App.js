082f44dc25f8379c58f0ce864bb7ae000f7bb285
HLiana Niehrah <liana.niehrah@gmail.com>
2019-10-19 09:48:59 -0700

    scaffolding of pages

--- frontend/src/pages/App.js
+++ frontend/src/pages/App.js
@@ -1,50 +1,18 @@
 import React from "react";
-import logo from "./logo.svg";
+import { BrowserRouter, Route } from "react-router-dom";
 import "../styles/App.css";
-
-import { Component } from "react";
-import ReactMapGL from "react-map-gl";
-
-class Map extends Component {
-  state = {
-    viewport: {
-      width: 400,
-      height: 400,
-      latitude: 37.7577,
-      longitude: -122.4376,
-      zoom: 8
-    }
-  };
-
-  render() {
-    const MAPBOX_URL = process.env.REACT_APP_MAPBOX;
-    return (
-      <ReactMapGL
-        {...this.state.viewport}
-        onViewportChange={viewport => this.setState({ viewport })}
-        mapboxApiAccessToken={MAPBOX_URL}
-      />
-    );
-  }
-}
+import MapPage from "./MapPage/MapPage";
+import Login from "./Login/Login";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Whats up people</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
-      <Map></Map>
+      <BrowserRouter>
+        <Route path="/" component={MapPage}></Route>
+        <Route path="/login" component={Login}></Route>
+        <Route path="/form" component={Form}></Route>
+        <Route path="/error" component={ErrorPage}></Route>
+      </BrowserRouter>
     </div>
   );
 }
