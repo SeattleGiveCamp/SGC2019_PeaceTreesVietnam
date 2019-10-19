5313dcb4a7825cb4c02d86a0f77b7d4f453b1e20
Erin Trainor <trainor.erin@gmail.com>
2019-10-19 11:12:23 -0700

    dynamic select component complete

--- frontend/src/pages/Form/Form.js
+++ frontend/src/pages/Form/Form.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React from 'react';
+import ProjectForm from '../../components/ProjectForm/ProjectForm';
 
 export default class Header extends React.Component {
   state = {};
 
   render() {
-    return <div>Header</div>;
+    return <ProjectForm />;
   }
 }
