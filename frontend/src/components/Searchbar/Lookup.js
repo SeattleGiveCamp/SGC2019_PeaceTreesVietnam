f41bdd8772692935790a00ebf1ca5b949259a7b1
kimkablitz <kim.tran549@gmail.com>
2019-10-19 13:19:42 -0700

    some data

--- frontend/src/components/Searchbar/Lookup.js
+++ frontend/src/components/Searchbar/Lookup.js
@@ -16,6 +16,7 @@ const suggestions =[
     value: suggestion.phrase,
     label: "[Found: " + suggestion.establishment + "] " + suggestion.phrase,
 }));
+
 const styles = theme => ({
     root: {
       flexGrow: 1,
@@ -62,3 +63,144 @@ const styles = theme => ({
       height: theme.spacing.unit * 2,
     },
   });
+  
+  function NoOptionsMessage(props) {
+    return (
+      <Typography
+        color="textSecondary"
+        className={props.selectProps.classes.noOptionsMessage}
+        {...props.innerProps}
+      >
+        {props.children}
+      </Typography>
+    );
+  }
+  
+  function inputComponent({ inputRef, ...props }) {
+    return <div ref={inputRef} {...props} />;
+  }
+  
+  function Control(props) {
+    return (
+      <TextField
+        fullWidth
+        InputProps={{
+          inputComponent,
+          inputProps: {
+            className: props.selectProps.classes.input,
+            inputRef: props.innerRef,
+            children: props.children,
+            ...props.innerProps,
+          },
+        }}
+        {...props.selectProps.textFieldProps}
+      />
+    );
+  }
+  
+  function Option(props) {
+    return (
+      <MenuItem
+        buttonRef={props.innerRef}
+        selected={props.isFocused}
+        component="div"
+        style={{
+          fontWeight: props.isSelected ? 500 : 400,
+        }}
+        {...props.innerProps}
+      >
+        {props.children}
+      </MenuItem>
+    );
+  }
+  
+  function Placeholder(props) {
+    return (
+      <Typography
+        color="textSecondary"
+        className={props.selectProps.classes.placeholder}
+        {...props.innerProps}
+      >
+        {props.children}
+      </Typography>
+    );
+  }
+  
+  function SingleValue(props) {
+    return (
+      <Typography className={props.selectProps.classes.singleValue} {...props.innerProps}>
+        {props.children}
+      </Typography>
+    );
+  }
+  
+  function ValueContainer(props) {
+    return <div className={props.selectProps.classes.valueContainer}>{props.children}</div>;
+  }
+  
+  function Menu(props) {
+    return (
+      <Paper square className={props.selectProps.classes.paper} {...props.innerProps}>
+        {props.children}
+      </Paper>
+    );
+  }
+  
+  const components = {
+    Control,
+    Menu,
+    NoOptionsMessage,
+    Option,
+    Placeholder,
+    SingleValue,
+    ValueContainer,
+  };
+  
+  class IntegrationReactSelect extends React.Component {
+    state = {
+      single: null,
+    };
+  
+    handleChange = name => value => {
+      this.setState({
+        [name]: value,
+      });
+    };
+  
+    render() {
+      const { classes, theme } = this.props;
+  
+      const selectStyles = {
+        input: base => ({
+          ...base,
+          color: theme.palette.text.primary,
+          '& input': {
+            font: 'inherit',
+          },
+        }),
+      };
+  
+      return (
+        <div className={classes.root}>
+          <NoSsr>
+            <Select
+              classes={classes}
+              styles={selectStyles}
+              options={suggestions}
+              components={components}
+              value={this.state.single}
+              onChange={this.handleChange('single')}
+              placeholder="Search for difficult to categorize items"
+            />
+          </NoSsr>
+        </div>
+      );
+    }
+  }
+  
+  IntegrationReactSelect.propTypes = {
+    classes: PropTypes.object.isRequired,
+    theme: PropTypes.object.isRequired,
+  };
+  
+  export default withStyles(styles, { withTheme: true })(IntegrationReactSelect);
