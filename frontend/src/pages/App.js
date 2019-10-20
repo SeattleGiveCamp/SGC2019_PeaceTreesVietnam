abcce0777cec312a7ab38625995058f5a836ff9b
Erin Trainor <trainor.erin@gmail.com>
2019-10-19 19:26:26 -0700

    form might be working??

--- frontend/src/pages/App.js
+++ frontend/src/pages/App.js
@@ -1,23 +1,23 @@
-import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
-import "../styles/App.css";
-import MapPage from "./MapPage/MapPage";
-import Login from "./Login/Login";
-import Form from "./Form/Form";
-import ErrorPage from "./ErrorPage/ErrorPage";
-import OrdnancePage from "./OrdnancePage/OrdnancePage";
-import AdminPage from "./AdminPage/AdminPage";
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import '../styles/App.css';
+import MapPage from './MapPage/MapPage';
+import Login from './Login/Login';
+import Form from './Form/Form';
+import ErrorPage from './ErrorPage/ErrorPage';
+import OrdnancePage from './OrdnancePage/OrdnancePage';
+import AdminPage from './AdminPage/AdminPage';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <BrowserRouter>
-        <Route path="/" component={MapPage}></Route>
-        <Route path="/login" component={Login}></Route>
-        <Route path="/form" component={Form}></Route>
-        <Route path="/error" component={ErrorPage}></Route>
-        <Route path="/ordnance" component={OrdnancePage}></Route>
-        <Route path="/admin" component={AdminPage}></Route>
+        <Route path='/map' component={MapPage}></Route>
+        <Route path='/login' component={Login}></Route>
+        <Route path='/form' component={Form}></Route>
+        <Route path='/error' component={ErrorPage}></Route>
+        <Route path='/ordnance' component={OrdnancePage}></Route>
+        <Route path='/admin' component={AdminPage}></Route>
       </BrowserRouter>
     </div>
   );
